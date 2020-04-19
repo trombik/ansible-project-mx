f43f2384814db7ef30a473e484de54397a1a2917
Tomoyuki Sakurai <y@trombik.org>
2020-04-19 23:59:21 +0900

    bugfix: remove hard-coded test data from tests

--- spec/serverspec/mx/000_default_spec.rb
+++ spec/serverspec/mx/000_default_spec.rb
@@ -7,13 +7,24 @@ describe fqdn do
   it_behaves_like "a host with default users"
 end
 
+def virtual_users(file)
+  begin
+    YAML.safe_load(Ansible::Vault.decrypt(file: file))["project_virtual_user_credentials"]
+  rescue RuntimeError
+    YAML.load_file(file)["project_virtual_user_credentials"]
+  end
+end
+
 describe file "/etc/mail/passwd" do
   it { should exist }
   it { should be_file }
   it { should be_mode 640 }
   it { should be_owned_by "root" }
   it { should be_grouped_into "vmailauth" }
-  its(:content) { should match(/^john@trombik\.org:\$2b\$08\$.*::::::$/) }
+  virtual_users("playbooks/group_vars/#{test_environment}-credentials.yml").each do |v|
+    uname = v.split(":").first
+    its(:content) { should match(/^#{uname}:\$[0-9a-z]{2}\$[0-9]{2}\$.*::::::$/) }
+  end
 end
 
 describe service "smtpd" do
