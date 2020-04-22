ecee6b54d4f2024d25a7b8f154fe9895fd5503e1
Tomoyuki Sakurai <y@trombik.org>
2020-04-22 13:57:16 +0900

    bugfix: remove ec2-user, not vagrant

--- terraform/plans/staging/openbsd_user_data.sh
+++ terraform/plans/staging/openbsd_user_data.sh
@@ -2,8 +2,8 @@
 pkg_add python%3.7 curl sudo--
 echo "#includedir /etc/sudoers.d" >> /etc/sudoers
 mkdir -p /etc/sudoers.d
-touch /etc/sudoers.d/vagrant
-chmod 440 /etc/sudoers.d/vagrant
+touch /etc/sudoers.d/ec2-user
+chmod 440 /etc/sudoers.d/ec2-user
 cat <<EOF > /etc/sudoers.d/ec2-user
 Defaults:ec2-user !requiretty
 ec2-user ALL=(ALL) NOPASSWD: ALL
