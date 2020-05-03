40caf358e6e1946628a987e07f3cd70915a8a88a
Tomoyuki Sakurai <y@trombik.org>
2020-05-03 15:49:33 +0900

    feature: implement prod

---
+++ inventories/prod/test_environment.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "rake"
+require "shellwords"
+
+# `staging` environment
+#
+# creates the environment in EC2, using terraform.
+class TestEnvironment
+  include FileUtils
+
+  def initialize; end
+
+  def plan_path
+    "terraform/plans/#{ansible_environment}"
+  end
+
+  def up
+    sh "terraform apply #{Shellwords.escape(plan_path)}"
+
+    # make sure the cache is up-to-date
+    sh "#{inventory_path + '/ec2.py'} --refresh-cache"
+
+    # make sure all hosts are ready for ansible play
+    retry_opts = {
+      max_tries: 10, base_sleep_seconds: 10, max_sleep_seconds: 30
+    }
+    with_retries(retry_opts) do |_attempt_number|
+      sh "ansible -i #{inventory_path} " \
+         "--ssh-common-args '-o \"UserKnownHostsFile /dev/null\" -o \"StrictHostKeyChecking no\"'" \
+         "--user #{Shellwords.escape(user)} -m ping all"
+    end
+  end
+
+  def clean
+    sh "terraform destroy #{Shellwords.escape(plan_path)}"
+  end
+
+  def provision
+    sh "ansible-playbook -i #{Shellwords.escape(inventory_path)} " \
+      "--ssh-common-args '-o \"UserKnownHostsFile /dev/null\" -o \"StrictHostKeyChecking no\"' " \
+      "--user #{Shellwords.escape(user)} playbooks/site.yml"
+  end
+
+  def prepare
+    # NOOP
+  end
+
+  def user
+    ENV["PROJECT_USER"] || ENV["USER"]
+  end
+end
