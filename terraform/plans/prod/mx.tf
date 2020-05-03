b33207de17b88368d047e7edd0d471b766171ea9
Tomoyuki Sakurai <y@trombik.org>
2020-05-03 15:49:33 +0900

    feature: implement prod

--- terraform/plans/prod/mx.tf
+++ terraform/plans/prod/mx.tf
@@ -14,23 +14,83 @@ variable "stage" {
 }
 
 variable "fqdn" {
-  type = "string"
+  type = string
   default = "mx1.trombik.org"
 }
 
-module "instance" {
-  source                        = "git::https://github.com/cloudposse/terraform-aws-ec2-instance.git?ref=0.7.0"
-  ssh_key_pair                  = "terraform-key"
-  instance_type                 = "t2.micro"
-  ami                           = "ami-db1299bd"
+variable "ports_tcp_public" {
+  type = list(number)
+  default = [22, 25, 53, 80, 443, 587, 993]
+}
+
+variable "ports_udp_public" {
+  type = list(number)
+  default = [53]
+}
+
+resource "aws_security_group" "mx" {
+  name                          = "security_group_mx"
+  description                   = "Security group for MX host in project MX"
   vpc_id                        = "vpc-7a87641e"
-  subnet                        = "subnet-293b9c5f"
-  assign_eip_address            = "false"
-  name                          = "${replace(var.fqdn, "[.]", "_")}"
-  namespace                     = "${module.project_yaml.result["name"]}"
-  stage                         = "${var.stage}"
-  allowed_ports                 = ["22", "25", "587", "993"]
-  tags                          = "${map("Name", var.fqdn, "Project", module.project_yaml.result["name"])}"
-  root_volume_type              = "standard"
-  user_data                     = "${file("${path.module}/openbsd_user_data.sh")}"
+  tags                          = { "Stage" = var.stage }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  dynamic "ingress" {
+    for_each = var.ports_tcp_public
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = var.ports_udp_public
+    iterator = udp_port
+    content {
+      from_port = udp_port.value
+      to_port   = udp_port.value
+      protocol  = "udp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip_association" "mx_eip" {
+  instance_id   = aws_instance.mx.id
+  allocation_id = "eipalloc-048c8b37f98e53147"
+}
+
+resource "aws_instance" "mx" {
+  key_name                      = "terraform-key"
+  instance_type                 = "t2.micro"
+  ami                           = "ami-04af7ec1b9ea369dd"
+  vpc_security_group_ids        = ["${aws_security_group.mx.id}"]
+  subnet_id                     = "subnet-293b9c5f"
+  tags                          = { "Name" = var.fqdn, "Project" = module.project_yaml.result["name"], "Stage" = var.stage }
+  user_data                     = file("${path.module}/openbsd_user_data.sh")
+  root_block_device {
+    delete_on_termination = false
+    volume_type = "standard"
+    volume_size = 30
+  }
 }
