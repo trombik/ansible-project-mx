5114805e0c8ca3a7d6b687459bb6b4dd90a04b55
Tomoyuki Sakurai <y@trombik.org>
2022-04-01 13:15:31 +0700

    bugfix: replace deprecated with_clean_env
    with with_original_env

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -3,7 +3,7 @@
 require_relative "../spec_helper"
 require "ansible/vault"
 
-# XXX `vagrant` command must be called within `with_clean_env`, not only
+# XXX `vagrant` command must be called within `with_original_env`, not only
 # `vagrant` command in this file, but also all other invocations in other
 # places, such as libraries that depend on `vagrant` command, and spec files.
 
@@ -12,7 +12,7 @@ ENV["VAGRANT_CWD"] = Pathname.new(File.dirname(__FILE__)).parent.parent.to_s
 # XXX inject vagrant `bin` path to ENV["PATH"]
 # https://github.com/reallyenglish/packer-templates/pull/48
 vagrant_path = ""
-Bundler.with_clean_env do
+Bundler.with_original_env do
   gem_which_vagrant = `gem which vagrant 2>/dev/null`.chomp
   if gem_which_vagrant != ""
     vagrant_path = Pathname
@@ -28,7 +28,7 @@ ENV["PATH"] = "#{vagrant_path}:#{ENV['PATH']}"
 # @return [String] output of `vagrant status`
 def vagrant_status
   out = ""
-  Bundler.with_clean_env do
+  Bundler.with_original_env do
     out = `vagrant status --machine-readable`
     raise StandardError, "Failed to run vagrant status" unless $CHILD_STATUS.exitstatus.zero?
   end
